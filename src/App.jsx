1aa7bf179a4b079555e7bd44d3b9254b9fcbd2df
Derek Wan <d.wan@icloud.com>
2023-05-15 20:29:28 +0800

    Finish lesson 2 (adding interactivity)

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,60 @@
-import { people } from "./data";
-import { getImageUrl } from "./utils";
+import { useImmer } from "use-immer";
 
-export default function List() {
-  const func = (person) => {
-    return (
-      <li key={person.id}>
-        <img src={getImageUrl(person)} alt={person.name} />
-        <p>
-          <b>{person.name}:</b>
-          {" " + person.profession + " "}
-          known for {person.accomplishment}
-        </p>
-      </li>
-    );
-  };
-  const listItems = people.map(func);
+export default function Form() {
+  const [person, updatePerson] = useImmer({
+    name: "Niki de Saint Phalle",
+    artwork: {
+      title: "Blue Nana",
+      city: "Hamburg",
+      image: "https://i.imgur.com/Sd1AgUOm.jpg",
+    },
+  });
+
+  function handleNameChange(e) {
+    updatePerson((p) => {
+      p.name = e.target.value;
+    });
+  }
+  function handleTitleChange(e) {
+    updatePerson((p) => {
+      p.artwork.title = e.target.value;
+    });
+  }
+
+  function handleCityChange(e) {
+    updatePerson((p) => {
+      p.artwork.city = e.target.value;
+    });
+  }
+
+  function handleImageChange(e) {
+    updatePerson((p) => {
+      p.artwork.image = e.target.value;
+    });
+  }
+
   return (
-    <article>
-      <h1>Scientists</h1>
-      <ul>{listItems}</ul>
-    </article>
+    <>
+      <label>
+        Name: <input value={person.name} onChange={handleNameChange} />
+      </label>
+      <label>
+        Title:{" "}
+        <input value={person.artwork.title} onChange={handleTitleChange} />
+      </label>
+      <label>
+        City: <input value={person.artwork.city} onChange={handleCityChange} />
+      </label>
+      <label>
+        Image:{" "}
+        <input value={person.artwork.image} onChange={handleImageChange} />
+      </label>
+      <p>
+        <i>{person.artwork.title}</i> by {person.name}
+        <br />
+        (location in {person.artwork.city})
+      </p>
+      <img src={person.artwork.image} alt={person.artwork.title} />
+    </>
   );
 }
