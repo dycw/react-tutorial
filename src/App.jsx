97218be5a17de0080869509fd4619410b6dd7d32
Derek Wan <d.wan@icloud.com>
2023-05-15 19:36:52 +0800

    Finish lesson 1 (describing the UI) (#6)

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
-import Profile from "./Profile";
+import { people } from "./data";
+import { getImageUrl } from "./utils";
 
-export default function Gallery() {
+export default function List() {
+  const func = (person) => {
+    return (
+      <li key={person.id}>
+        <img src={getImageUrl(person)} alt={person.name} />
+        <p>
+          <b>{person.name}:</b>
+          {" " + person.profession + " "}
+          known for {person.accomplishment}
+        </p>
+      </li>
+    );
+  };
+  const listItems = people.map(func);
   return (
-    <section>
-      <h1>Amazing scientists</h1>
-      <Profile />
-      <Profile />
-      <Profile />
-    </section>
+    <article>
+      <h1>Scientists</h1>
+      <ul>{listItems}</ul>
+    </article>
   );
 }
