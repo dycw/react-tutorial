eb62e4fe90a1782b6d5fbc0fc16d9c49332747db
Derek Wan <d.wan@icloud.com>
2023-05-18 00:32:41 +0800

    Migrate to `prettier-plugin-organize-imports`

--- src/TaskList.jsx
+++ src/TaskList.jsx
@@ -1,5 +1,5 @@
-import { TasksContext, TasksDispatchContext } from "./TasksContext";
 import { useContext, useState } from "react";
+import { TasksContext, TasksDispatchContext } from "./TasksContext";
 
 export default function TaskList() {
   const tasks = useContext(TasksContext);
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./styles.css";
-import React, { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
 
 const root = createRoot(document.getElementById("root"));
 root.render(
